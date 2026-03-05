c47acf89c53e6ed9b22753289a418d49ce3eb83d
aaronkychan <110659061+universallyleo@users.noreply.github.com>
2026-03-06 06:28:38 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,6 +50,10 @@
 		<a href="https://x.com/55gohan06">
 			<img width="32" src={xicon} alt="Twitter" />
 		</a>
+		&nbsp;&nbsp;
+		<a class="external" href="https://universallyleo.github.io/heroinesCrowdfund/">
+			ヒロインズクラファ記録
+		</a>
 	</footer>
 </div>
 
@@ -62,6 +66,17 @@
 
 	#hiddenNavToggle {
 		display: none;
+	}
+
+	a.external::after {
+		content: '';
+		display: inline-block;
+		background-repeat: no-repeat;
+		background-size: contain;
+		background-position: center;
+		width: 16px;
+		height: 16px;
+		background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path fill='%238d8d8d' d='M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1M14 4c0 .55.45 1 1 1h2.59l-9.13 9.13a.996.996 0 1 0 1.41 1.41L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1'/></svg>");
 	}
 
 	nav {
